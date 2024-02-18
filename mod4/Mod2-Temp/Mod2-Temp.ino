a7b27556d66e5cf303409288a1233371c48bdac6
AlexKShah <AlexKShah@users.noreply.github.com>
2024-02-18 18:06:22 -0500

    Create Mod2-Temp.ino
    arduino files for project 2

---
+++ mod4/Mod2-Temp/Mod2-Temp.ino
@@ -0,0 +1,52 @@
+/*
+Project 2 - Temperature Sensor with Interrupts
+Alex Shah
+2/18/24
+*/
+
+//using a tmp36gz on pin a0
+int iPin = A0;
+
+volatile float temperatureF = 0;
+volatile bool newReadingAvailable = false; 
+
+void setup() {
+  Serial.begin(9600);
+  //use AREF port to enhance precision
+  analogReference(EXTERNAL);
+  attachInterrupt(digitalPinToInterrupt(2), readTemperature, FALLING);
+  cli(); //suspend interrupts
+  //clear timers
+  TCCR1A = 0;
+  TCCR1B = 0;
+  //set cs12, cs10 bits for prescalar 1024
+  TCCR1B |= (1<<CS12)| (0<<CS11) | (1<<CS10);
+  //use compare match by setting OCIE1A to 1
+  TIMSK1 |= B00000010;
+  //set compare register A ~ 5 sec
+  OCR1A = 15625;
+  sei(); //resume interrupts
+}
+
+void loop() {
+  if (newReadingAvailable) {
+    Serial.println(temperatureF);
+    newReadingAvailable = false;
+  }
+}
+
+void readTemperature() {
+  int sensorValue = analogRead(iPin); 
+  //external reference voltage and resolution
+  float fRef = 3.3;
+  float fRes = 1024.0;
+  // celsuis = (sensor value * (voltage in mw / resolution) - offset) * 100
+  float fVolt = sensorValue * (fRef / fRes);
+  float fTempC = (fVolt - 0.5) * 100;
+  temperatureF = (fTempC * 9 / 5) + 32;
+  newReadingAvailable = true;
+}
+
+ISR(TIMER1_COMPA_vect) {
+  readTemperature();
+}
